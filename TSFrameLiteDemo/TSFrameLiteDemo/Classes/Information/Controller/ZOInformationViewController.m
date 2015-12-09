f343c9f70997cf1ccab8f02a05b1e9d5926aef28
techsen <chenyusen@51talk.com>
2015-12-09 19:06:09 +0800

    bug fix

--- TSFrameLiteDemo/TSFrameLiteDemo/Classes/Information/Controller/ZOInformationViewController.m
+++ TSFrameLiteDemo/TSFrameLiteDemo/Classes/Information/Controller/ZOInformationViewController.m
@@ -24,7 +24,7 @@
     ZOInfomationRequestModel *param = [[ZOInfomationRequestModel alloc] init];
     param.v = @"4.0";
     param.class_id = @"0";
-    param.last_time = @"2015-12-08 16:18";
+//    param.last_time = @"2015-12-08 16:18";
     param.page = @"1";
     
     [ZOInfomationNetworkHelper getInformationListWithParam:param success:^(ZOInfomationResultModel *result) {
