9047ad536cd38562479b5202241e9e9d8d590a01
techsen <chenyusen@51talk.com>
2015-12-10 09:37:47 +0800

    代码提交

--- TSFrameLite/Network/TSNetWorkManager.h
+++ TSFrameLite/Network/TSNetWorkManager.h
@@ -7,7 +7,97 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "Singleton.h"
+#import <AFNetworking.h>
 @interface TSNetWorkManager : NSObject
+/** 项目请求的基本网络URL */
+@property(nonatomic, copy) NSString *baseUrl;
+/** 网络请求超时时间 */
+@property(nonatomic, assign) NSTimeInterval timeoutInterval;
+/** 接受的内容类型 */
+@property(nonatomic, strong) NSSet *acceptableContentTypes;
+/** 关闭网络日志打印 */
+@property(nonatomic, assign) BOOL closeLog;
 
+singleton_h(Manager);
+/**
+ *  get请求方法
+ *
+ *  @param url         请求url
+ *  @param param       请求参数模型/字典
+ *  @param resultClass 需要映射的对象
+ *  @param success     请求成功回调
+ *  @param failure     请求失败回调
+ *
+ *  @return 网络请求操作
+ */
++ (AFHTTPRequestOperation *)getWithUrl:(NSString *)url param:(id)param resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure;
+
+/**
+ *  post请求方法
+ *
+ *  @param url         请求url
+ *  @param param       请求参数模型/字典
+ *  @param resultClass 需要映射的对象
+ *  @param success     请求成功回调
+ *  @param failure     请求失败回调
+ *
+ *  @return 网络请求操作
+ */
++ (AFHTTPRequestOperation *)postWithUrl:(NSString *)url param:(id)param resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure;
+
+/**
+ *  get请求方法 自定义超时时间
+ *
+ *  @param url             请求url
+ *  @param param           请求参数模型/字典
+ *  @param timeoutInterval 超时时间
+ *  @param resultClass     需要映射的对象
+ *  @param success         请求成功回调
+ *  @param failure         请求失败回调
+ *
+ *  @return 网络请求操作
+ */
++ (AFHTTPRequestOperation *)getWithUrl:(NSString *)url param:(id)param timeoutInterval:(NSTimeInterval)timeoutInterval resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure;
+
+/**
+ *  post请求方法 自定义超时时间
+ *
+ *  @param url             请求url
+ *  @param param           请求参数模型/字典
+ *  @param timeoutInterval 超时时间
+ *  @param resultClass     需要映射的对象
+ *  @param success         请求成功回调
+ *  @param failure         请求失败回调
+ *
+ *  @return 网络请求操作
+ */
++ (AFHTTPRequestOperation *)postWithUrl:(NSString *)url param:(id)param timeoutInterval:(NSTimeInterval)timeoutInterval resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure;
+
+@end
+
+
+@interface TSBaseNetWorkManager : NSObject
+
+/**
+ *  get请求方式
+ *
+ *  @param url             请求url
+ *  @param params          请求参数字典
+ *  @param timeoutInterval 超时时间
+ *  @param success         请求成功回调
+ *  @param failure         请求失败回调
+ */
++ (AFHTTPRequestOperation *)get:(NSString *)url params:(NSDictionary *)params timeoutInterval:(NSTimeInterval)timeoutInterval success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure;
+
+/**
+ *  post请求方式
+ *
+ *  @param url             请求url
+ *  @param params          请求参数字典
+ *  @param timeoutInterval 超时时间
+ *  @param success         请求成功回调
+ *  @param failure         请求失败回调
+ */
++ (AFHTTPRequestOperation *)post:(NSString *)url params:(NSDictionary *)params timeoutInterval:(NSTimeInterval)timeoutInterval success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure;
 @end
--- TSFrameLite/Network/TSNetWorkManager.m
+++ TSFrameLite/Network/TSNetWorkManager.m
@@ -7,7 +7,127 @@
 //
 
 #import "TSNetWorkManager.h"
+#import <MJExtension.h>
+#import "TSHeader.h"
 
 @implementation TSNetWorkManager
+singleton_m(Manager);
 
++ (AFHTTPRequestOperation *)getWithUrl:(NSString *)url param:(id)param resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure {
+    return [self getWithUrl:url param:param timeoutInterval:[TSNetWorkManager sharedManager].timeoutInterval resultClass:resultClass success:success failure:failure];
+}
+
++ (AFHTTPRequestOperation *)postWithUrl:(NSString *)url param:(id)param resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure {
+    return [self postWithUrl:url param:param timeoutInterval:[TSNetWorkManager sharedManager].timeoutInterval resultClass:resultClass success:success failure:failure];
+}
+
++ (AFHTTPRequestOperation *)getWithUrl:(NSString *)url param:(id)param timeoutInterval:(NSTimeInterval)timeoutInterval resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure {
+    return [TSBaseNetWorkManager get:[NSString stringWithFormat:@"%@%@",[TSNetWorkManager sharedManager].baseUrl?[TSNetWorkManager sharedManager].baseUrl:@"",url?url:@""] params:[param isKindOfClass:[NSDictionary class]]?param:[param mj_keyValues] timeoutInterval:timeoutInterval>0?timeoutInterval:10 success:^(id responseObj) {
+        if (success) {
+            if (responseObj) {
+                id result = [resultClass mj_objectWithKeyValues:responseObj];
+                success(result);
+            }
+        }
+    } failure:^(NSError *error) {
+        if (failure) {
+            failure(error);
+        }
+    }];
+    
+
+}
+
++ (AFHTTPRequestOperation *)postWithUrl:(NSString *)url param:(id)param timeoutInterval:(NSTimeInterval)timeoutInterval resultClass:(Class)resultClass success:(void(^)(id responseObj))success failure:(void(^)(NSError *error)) failure {
+    return [TSBaseNetWorkManager post:[NSString stringWithFormat:@"%@%@",[TSNetWorkManager sharedManager].baseUrl?[TSNetWorkManager sharedManager].baseUrl:@"",url?url:@""] params:[param isKindOfClass:[NSDictionary class]]?param:[param mj_keyValues] timeoutInterval:timeoutInterval>0?timeoutInterval:10 success:^(id responseObj) {
+        if (success) {
+            if (responseObj) {
+                id result = [resultClass mj_objectWithKeyValues:responseObj];
+                success(result);
+            }
+        }
+    } failure:^(NSError *error) {
+        if (failure) {
+            failure(error);
+        }
+    }];
+}
+
+@end
+
+
+@implementation TSBaseNetWorkManager
+
++ (AFHTTPRequestOperation *)get:(NSString *)url params:(NSDictionary *)params timeoutInterval:(NSTimeInterval)timeoutInterval success:(void (^)(id))success failure:(void (^)(NSError *))failure {
+    NSInteger romdomCount = 0;
+    if (![TSNetWorkManager sharedManager].closeLog) {
+        romdomCount = arc4random();
+        TSLog(@"url(%ld) ===============>\n%@\nparams = %@",romdomCount,url,params);
+    }
+    
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+
+    manager.responseSerializer.acceptableContentTypes = [TSNetWorkManager sharedManager].acceptableContentTypes.count?[TSNetWorkManager sharedManager].acceptableContentTypes:[NSSet setWithArray:@[@"text/html",@"application/json"]];
+//    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    manager.requestSerializer.timeoutInterval = timeoutInterval;
+    
+    return [manager GET:url parameters:params success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        if (![operation isCancelled]) {
+            if (success) {
+                if (![TSNetWorkManager sharedManager].closeLog) {
+                    TSLog(@"response(%ld) ===============> %@",romdomCount,responseObject);
+                }
+                success(responseObject);
+            }
+        }
+        
+    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+        if (![operation isCancelled]) {
+            if (failure) {
+                if (![TSNetWorkManager sharedManager].closeLog) {
+                    TSLog(@"error(%ld) ===============> %@",romdomCount,error);
+                }
+                failure(error);
+            }
+        }
+    }];
+}
+    
++ (AFHTTPRequestOperation *)post:(NSString *)url params:(NSDictionary *)params timeoutInterval:(NSTimeInterval)timeoutInterval success:(void (^)(id))success failure:(void (^)(NSError *))failure {
+    NSInteger romdomCount = 0;
+    if (![TSNetWorkManager sharedManager].closeLog) {
+        romdomCount = arc4random();
+        TSLog(@"url(%ld) ===============>\n%@\nparams = %@",romdomCount,url,params);
+        
+    }
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes = [TSNetWorkManager sharedManager].acceptableContentTypes.count?[TSNetWorkManager sharedManager].acceptableContentTypes:[NSSet setWithArray:@[@"text/html",@"application/json"]];
+        manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    manager.requestSerializer.timeoutInterval = timeoutInterval;
+    
+    return [manager POST:url parameters:params success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        if (![operation isCancelled]) {
+            if (success) {
+                if (![TSNetWorkManager sharedManager].closeLog) {
+                    TSLog(@"response(%ld) ===============> %@",romdomCount,responseObject);
+                }
+                success(responseObject);
+            }
+        }
+        
+    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+        if (![operation isCancelled]) {
+            if (failure) {
+                if (![TSNetWorkManager sharedManager].closeLog) {
+                    TSLog(@"error(%ld) ===============> %@",romdomCount,error);
+                }
+                failure(error);
+            }
+        }
+    }];
+}
 @end
